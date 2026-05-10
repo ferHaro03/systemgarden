fd6d7b5bac466278f7daa83db4832c71a6dee791
Victor <vicene126@gmail.com>
2026-05-10 10:17:46 -0700

    Victor Actualizado

--- Victor/print.asm
+++ Victor/print.asm
@@ -1,10 +1,17 @@
+include print.lib
 .model small
 .stack
 .data            
     interfaz    db 'A continuacion las notificaciones', 10,13
                 db 'Presiones 1 para salir [ ]$'  
                 
-    car         db 0
+    car         db 0    
+    
+    msjAnomalia db 'Se ha detectado una anomalia', 10,13
+    msjRevCult  db 'Es necesario revisar las plantaciones', 10,13
+    msjRevVent  db 'Es necesario revisar la ventilacion', 10,13
+    msjRevFuga  db 'Se ha detectado una fuga de calor', 10,13
+    msjPeligro  db 'Cultivos en peligro, revisar pronto', 10,13  
     
 .code 
     mov ax, @data
@@ -13,14 +20,15 @@
     
     interfazir:
         imp_sincolor interfaz
-        
-        ;notificaciones
-        
+         
+        mrevcult
+         
         cursor 1, 22, 0 
         leerCaracter car
         cmp car, 1
         je fin
     
     fin:
-        
+        mov ax, 4c00h
+        int 21h    
 end  
